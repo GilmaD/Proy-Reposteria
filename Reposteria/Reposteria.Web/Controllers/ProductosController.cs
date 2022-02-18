df8a31682d70841a7fac08485d096ccc4df3c4dd
GilmaD <84815208+GilmaD@users.noreply.github.com>
2022-02-18 01:07:06 -0600

    Add files via upload

--- Reposteria/Reposteria.Web/Controllers/ProductosController.cs
+++ Reposteria/Reposteria.Web/Controllers/ProductosController.cs
@@ -5,17 +5,70 @@ using System.Linq;
 using System.Web;
 using System.Web.Mvc;
 
-namespace Reposteria.Web.Controllers
+namespace Reposteria.WebAdmin.Controllers
 {
     public class ProductosController : Controller
     {
+        ProductosBL _productosBL;
+
+        public ProductosController()
+        {
+            _productosBL = new ProductosBL();
+        }
         // GET: Productos
         public ActionResult Index()
         {
-            var productosBL = new ProductosBL();
-            var listadeProductos = productosBL.ObtenerProductos();
-
+            var listadeProductos = _productosBL.ObtenerProductos();
+            
             return View(listadeProductos);
+        }
+
+        public ActionResult Crear()
+        {
+            var nuevoProducto = new Producto(); 
+
+            return View(nuevoProducto);
+        }
+        [HttpPost]
+        public ActionResult Crear(Producto producto)
+        {
+            _productosBL.GuardarProducto(producto);
+             
+            return RedirectToAction("Index");
+        }
+
+        public ActionResult Editar(int id)
+        {
+            var producto = _productosBL.ObtenerProducto(id);
+            return View();
+        }
+        [HttpPost]
+        public ActionResult Editar(Producto producto)
+        {
+            _productosBL.GuardarProducto(producto);
+
+            return RedirectToAction("Index");
+        }
+
+        public ActionResult Detalle(int id)
+        {
+            var producto = _productosBL.ObtenerProducto(id);
+
+            return View(producto);
+        }
+
+        public ActionResult Eliminar(int id)
+        {
+            var producto = _productosBL.ObtenerProducto(id);
+
+            return View(producto);
+        }
+        [HttpPost]
+        public ActionResult Eliminar(Producto producto)
+        {
+            _productosBL.EliminarProducto(producto.Id);
+
+            return RedirectToAction("Index");
         }
     }
 }
