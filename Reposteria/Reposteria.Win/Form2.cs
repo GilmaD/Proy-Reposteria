b38eee41b58a552c4dbe3b43a6b86bde92294dd5
GilmaD <84815208+GilmaD@users.noreply.github.com>
2022-03-26 01:08:02 -0600

    Add files via upload

---
+++ Reposteria/Reposteria.Win/Form2.cs
@@ -0,0 +1,37 @@
+﻿using Reposteria.BL;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Reposteria.Win
+{
+    public partial class Form2 : Form
+    {
+        ReporteVentasPorProductoBL _reporteVentasPorProductosBL;
+
+        public Form2()
+        {
+            InitializeComponent();
+            _reporteVentasPorProductosBL = new ReporteVentasPorProductoBL();
+
+            RefrescarDatos();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            RefrescarDatos();
+        }
+         private void RefrescarDatos()
+        {
+            var listadeVentasPorProducto = _reporteVentasPorProductosBL.ObtenerVentasPorProducto();
+            listadeVentasPorProductoBindingSource.DataSource = listadeVentasPorProducto;
+            listadeVentasPorProductoBindingSource.ResetBindings(false);
+        }
+    }
+}
